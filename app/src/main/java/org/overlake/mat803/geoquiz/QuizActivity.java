260611a73ab9d6bfa53ea6f1494e0e2f3d071e27
Adam Goodman <59903108+noticemeaikas@users.noreply.github.com>
2020-01-31 11:50:05 -0800

    NOTICE ME CHRIS!!! I also didn't know what to do with the fact that the app crashed whenever current index was below 0 (with the new previous button), so I fixed it. Index now goes to mQuestionBank.length - 1.

--- app/src/main/java/org/overlake/mat803/geoquiz/QuizActivity.java
+++ app/src/main/java/org/overlake/mat803/geoquiz/QuizActivity.java
@@ -1,8 +1,11 @@
 package org.overlake.mat803.geoquiz;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,15 +17,18 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPrevButton;
     private TextView mQuestionTextView;
     private Question[] mQuestionBank;
     private int mCurrentIndex;
     public static final String TAG = "QuizActivity";
+    public static final String KEY_INDEX = "currentIndex";
 
     public QuizActivity(){
         mQuestionBank = new Question[]{
                 new Question(R.string.question_australia, true),
                 new Question(R.string.question_oceans, true),
+                new Question(R.string.question_chris, true),
                 new Question(R.string.question_mideast, false),
                 new Question(R.string.question_africa, false),
                 new Question(R.string.question_americas, true),
@@ -33,8 +39,12 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
         setContentView(R.layout.activity_quiz);
-        Log.d(TAG, "onCreate() called");
+        Log.d(TAG, "onCreate() called()");
+
         mQuestionTextView = findViewById(R.id.question_text_view);
         updateQuestion();
 
@@ -56,6 +66,18 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
+        mPrevButton = findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                if(mCurrentIndex == -1) {
+                    mCurrentIndex = mQuestionBank.length - 1;
+                }
+                updateQuestion();
+            }
+        });
+
         mNextButton = findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +86,15 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
     }
 
     private void updateQuestion(){
@@ -86,32 +117,29 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart() called");
-    }
-
-    @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume() called");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause() called");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop() called");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstantState() called()");
+        outState.putInt(KEY_INDEX, mCurrentIndex);
     }
 }
